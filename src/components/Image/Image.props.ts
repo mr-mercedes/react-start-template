efc5b09704c42dfc7bccd34c107d52e64efc444b
mr-mercedes <99349035+mr-mercedes@users.noreply.github.com>
2024-02-29 20:10:59 +0000

    Deploying to gh-pages from @ mr-mercedes/mr-mercedes.github.io@263782d569aa967c8ebd72fe5397f67de23381aa 🚀

--- src/components/Image/Image.props.ts
+++
@@ -1,8 +0,0 @@
-import { DetailedHTMLProps, HTMLAttributes } from 'react';
-
-export interface ImageProps extends DetailedHTMLProps<HTMLAttributes<HTMLImageElement>, HTMLImageElement> {
-  src: string;
-  alt: string;
-  weight: number;
-  height: number;
-}
--- src/components/Image/Image.tsx
+++
@@ -1,7 +0,0 @@
-import React, { JSX } from 'react';
-
-import { ImageProps } from './Image.props';
-
-export const Image = ({ src, alt, height, weight, className, ...props }: ImageProps): JSX.Element => {
-  return <img src={src} alt={alt} height={height} width={weight} className={className} {...props} />;
-};
